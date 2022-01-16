fd0e1e4473ab427c0063c863353b126532afb900
tajiri <tajitazi.1004@gmail.com>
2022-01-16 17:42:36 +0900

    APIから返される本文を加工

--- appli1/serializers.py
+++ appli1/serializers.py
@@ -2,6 +2,7 @@
 # シリアライザーはデータ の入出力を扱い、モデルへの橋渡しをするクラス
 # APIのリクエスト/レスポンスに特化した機能を提供
 
+from django.utils.html import linebreaks, urlize
 from rest_framework import serializers
 from .models import Category, Post
 
@@ -23,7 +24,11 @@ class SimplePostSerializer(serializers.ModelSerializer):
 # 記事詳細用シリアライザー
 class PostSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
+    main_text = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = '__all__'
+
+    def get_main_text(self, instance):
+        return urlize(linebreaks(instance.main_text))
